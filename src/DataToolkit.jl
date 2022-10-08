ce63a90eb80a5eb130111505f63407ea40bd643e
TEC <git@tecosaur.net>
2022-10-08 15:18:07 +0800

    Add DataToolkitBase and DataToolkitCommon + init

--- src/DataToolkit.jl
+++ src/DataToolkit.jl
@@ -1,5 +1,27 @@
 module DataToolkit
 
-# Write your package code here.
+using DataToolkitBase
+using DataToolkitCommon
+
+export loadcollection!, dataset, DataSet
+
+"""
+    init()
+Load the project-local `Data.toml` if it exists.
+"""
+function init()
+    project_dir = first(Base.load_path())
+    if !isdir(project_dir)
+        project_dir = dirname(project_dir)
+    end
+    project_data = joinpath(project_dir, "Data.toml")
+    if isfile(project_data)
+        loadcollection!(project_data)
+    end
+end
+
+function __init__()
+    init()
+end
 
 end
