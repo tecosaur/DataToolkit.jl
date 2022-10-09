58eae54a01e080b7c6512e5dec2460b0d6a17e03
TEC <git@tecosaur.net>
2022-10-09 17:22:19 +0800

    refactor(filesystem): Remove unused code

--- Common/src/storage/filesystem.jl
+++ Common/src/storage/filesystem.jl
@@ -1,10 +1,3 @@
-const FILESYSTEM_PATH_REPLACEMENTS =
-    Dict(r"^@__DIR__" => function (storage)
-             something(storage.dataset.collection.path, pwd())
-         end
-         )
-
-# TODO @__DIR__ substitutions, etc.
 function storage(storage::DataStorage{:filesystem}, ::Type{IO};
                  write::Bool=false)
     file = abspath(dirname(storage.dataset.collection.path),
