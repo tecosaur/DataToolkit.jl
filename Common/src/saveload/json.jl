43a663bef7e60419ca714aed68a1775e822f7533
TEC <git@tecosaur.net>
2022-10-09 23:40:59 +0800

    tweak(json): provide supportedtypes method

--- Common/src/saveload/json.jl
+++ Common/src/saveload/json.jl
@@ -3,6 +3,10 @@ function load(::DataLoader{:json}, from::IO, as::Type)
     JSON3.read(from)
 end
 
+supportedtypes(::DataLoader{:json}) =
+    QualifiedType.(["JSON3.Object", "JSON3.Array", String,
+                    Number, Boolean, Nothing])
+
 function save(writer::DataWriter{:json}, dest::IO, info)
     @use JSON3
     if get(writer, "pretty", false)
