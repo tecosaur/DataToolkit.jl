6e0d7503a5e4132aa32d0432e6d50a2af07d497e
TEC <git@tecosaur.net>
2022-10-10 17:14:59 +0800

    Soft-load data collections in init()

--- Main/src/DataToolkit.jl
+++ Main/src/DataToolkit.jl
@@ -6,17 +6,18 @@ using DataToolkitCommon
 export loadcollection!, dataset, DataSet
 
 """
-    init()
+    init(force::Bool=false)
 Load the project-local `Data.toml` if it exists.
+Unless `force` is set, the data collection is soft-loaded.
 """
-function init()
+function init(force::Bool=false)
     project_dir = first(Base.load_path())
     if !isdir(project_dir)
         project_dir = dirname(project_dir)
     end
     project_data = joinpath(project_dir, "Data.toml")
     if isfile(project_data)
-        loadcollection!(project_data)
+        loadcollection!(project_data, soft=!force)
     end
 end
 
