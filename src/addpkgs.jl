f7b31cb2baf02e88af2bf7df3449e31335fc5d49
TEC <git@tecosaur.net>
2022-10-11 17:59:55 +0800

    feat(addpkgs): convenience function/macro @addpkgs

---
+++ src/addpkgs.jl
@@ -0,0 +1,15 @@
+using Pkg
+
+macro addpkgs(pkgs::Symbol...)
+    :(addpkgs(@__MODULE__, $(collect(pkgs))))
+end
+
+function addpkgs(mod::Module, pkgs::Vector{Symbol})
+    project_deps = Pkg.project().dependencies
+    if length(pkgs) == 1 && first(pkgs) == :(*)
+        pkgs = Symbol.(keys(project_deps))
+    end
+    for pkg in pkgs
+        addpkg(mod, pkg, project_deps[String(pkg)])
+    end
+end
