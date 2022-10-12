b6adae4b0c29e937379415b2c9c6eb57e206d3bd
TEC <git@tecosaur.net>
2022-10-12 09:59:25 +0800

    feat(init): support initialising with module

--- Main/src/DataToolkit.jl
+++ Main/src/DataToolkit.jl
@@ -12,14 +12,14 @@ include("addpkgs.jl")
 Load the project-local `Data.toml` if it exists.
 Unless `force` is set, the data collection is soft-loaded.
 """
-function init(force::Bool=false)
+function init(mod::Module=Base.Main; force::Bool=false)
     project_dir = first(Base.load_path())
     if !isdir(project_dir)
         project_dir = dirname(project_dir)
     end
     project_data = joinpath(project_dir, "Data.toml")
     if isfile(project_data)
-        loadcollection!(project_data, soft=!force)
+        loadcollection!(project_data, mod, soft=!force)
     end
 end
 
