1ca9163cafd388fd17fd0b6801f1f95eeb92a8b1
TEC <git@tecosaur.net>
2022-10-17 12:51:18 +0800

    tweak(addpkgs): warn when unable to register pkg

--- src/addpkgs.jl
+++ src/addpkgs.jl
@@ -10,6 +10,10 @@ function addpkgs(mod::Module, pkgs::Vector{Symbol})
         pkgs = Symbol.(keys(project_deps))
     end
     for pkg in pkgs
-        DataToolkitBase.addpkg(mod, pkg, project_deps[String(pkg)])
+        if haskey(project_deps, String(pkg))
+            DataToolkitBase.addpkg(mod, pkg, project_deps[String(pkg)])
+        else
+            @warn "(@addpkgs) $mod does not have $pkg in its dependencies, skipping."
+        end
     end
 end
