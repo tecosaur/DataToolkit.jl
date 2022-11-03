cfb65fde2f1422f5175bc44269e7601940214718
TEC <git@tecosaur.net>
2022-11-03 23:43:56 +0800

    feat(passthrough): obtain supported types from DS
    This only works because in DataToolkitBase the data storage transformers
    are added to the DataSet before the loaders.

--- Common/src/saveload/passthrough.jl
+++ Common/src/saveload/passthrough.jl
@@ -1,4 +1,4 @@
-function load(::DataLoader{:passthrough}, from::Any, ::Type{<:Any})
+function load(::DataLoader{:passthrough}, from::T, ::Type{T}) where {T <: Any}
     from
 end
 
@@ -9,3 +9,6 @@ end
 function save(::DataWriter{:passthrough}, dest::IO, info::Any)
     write(dest, info)
 end
+
+supportedtypes(::Type{DataLoader{:passthrough}}, _::Dict{String, Any}, dataset::DataSet) =
+    reduce(vcat, getproperty.(dataset.storage, :support)) |> unique
