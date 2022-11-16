fbfceb94119285a5f752d4f71c64738a280ca5dd
TEC <git@tecosaur.net>
2022-11-16 18:21:18 +0800

    tweak(passthrough): rename "ident" to "source"

--- Common/src/storage/passthrough.jl
+++ Common/src/storage/passthrough.jl
@@ -1,11 +1,11 @@
 getstorage(storage::DataStorage{:passthrough}, T::Type) =
-    read(resolve(Identifier(get(storage, "ident"))), T)
+    read(resolve(Identifier(get(storage, "source"))), T)
 
 createpriority(::Type{<:DataStorage{:passthrough}}) = 60
 
 function create(::Type{<:DataStorage{:passthrough}}, source::String)
     if try resolve(parse(Identifier, source)); true catch _ false end
-        Dict{String, Any}("ident" => source)
+        Dict{String, Any}("source" => source)
     end
 end
 
