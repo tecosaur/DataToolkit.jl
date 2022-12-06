f3676a25655d4523e43154ce2b7ec052a6164fd2
TEC <git@tecosaur.net>
2022-12-07 00:49:17 +0800

    feat(passthrough): keep data identifier links
    It's important not to loose the link between the source data set and the
    passthrough storage.

--- Common/src/storage/passthrough.jl
+++ Common/src/storage/passthrough.jl
@@ -1,5 +1,5 @@
 getstorage(storage::DataStorage{:passthrough}, T::Type) =
-    read(resolve(Identifier(get(storage, "source"))), T)
+    read(resolve(parse(Identifier, get(storage, "source"))), T)
 
 createpriority(::Type{<:DataStorage{:passthrough}}) = 60
 
@@ -7,6 +7,18 @@ function create(::Type{<:DataStorage{:passthrough}}, source::String)
     if try resolve(parse(Identifier, source)); true catch _ false end
         Dict{String, Any}("source" => source)
     end
+end
+
+# Ensure that `passthrough` storage registers dependents in the AbstractTrees
+# interface, as well as contextual hashing.
+
+DataToolkitBase.add_datasets!(acc::Vector{Identifier}, storage::DataStorage{:passthrough}) =
+    DataToolkitBase.add_datasets!(acc, parse(Identifier, get(storage, "source")))
+
+function chash(collection::DataCollection, storage::DataStorage{:passthrough}, h::UInt)
+    sourceh = chash(collection, parse(Identifier, get(storage, "source")), h)
+    invoke(chash, Tuple{DataCollection, AbstractDataTransformer, UInt},
+           collection, storage, sourceh)
 end
 
 # TODO putstorage
