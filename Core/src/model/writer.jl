7b3b8ea3ecdeadea9d4d49bdab31fe09748e0890
TEC <git@tecosaur.net>
2022-12-24 00:49:00 +0800

    feat(writer): rewrite toml to use multiline string

--- Core/src/model/writer.jl
+++ Core/src/model/writer.jl
@@ -119,15 +119,60 @@ function natkeygen(key::String)
     end
 end
 
+"""
+    tomlreformat!(io::IO)
+Consume `io` representing a TOML file, and reformat it to improve readability.
+Currently this takes the form of the following changes:
+- Replace inline multi-line strings with multi-line toml strings.
+
+An IOBuffer containing the reformatted content is returned.
+
+The processing assumes that `io` contains `TOML.print`-formatted content.
+Should this not be the case, mangled TOML may be emitted.
+"""
+function tomlreformat!(io::IO)
+    out = IOBuffer()
+    bytesavailable(io) == 0 && seekstart(io)
+    for line in eachline(io)
+        # Check for multi-line candidates. Cases:
+        #  1. key = "string..."
+        #  2. 'key...' = "string..."
+        #  3. "key..." = "string..."
+        if !isnothing(match(r"^\s*[\"'A-Za-z0-9_-].*= * \".*\"$", line))
+            write(out, line[1:findfirst(!isspace, line)-1]) # apply indent
+            key, value = first(TOML.parse(line))
+            if length(value) < 40 || count('\n', value) < 3
+                TOML.print(out, Dict{String, Any}(key => value))
+            elseif !occursin("'''", value) && count('"', value) > 4 &&
+                !any(c -> c != '\n' && Base.iscntrl(c), value)
+                TOML.Internals.Printer.printkey(out, [key])
+                write(out, " = '''\n", value, "''''\n")
+            else
+                TOML.Internals.Printer.printkey(out, [key])
+                write(out, " = \"\"\"\n",
+                      replace(sprint(TOML.Internals.Printer.print_toml_escaped, value),
+                              "\\n" => '\n'),
+                      "\"\"\"\n")
+            end
+        else
+            write(out, line, '\n')
+        end
+    end
+    out
+end
+
 function Base.write(io::IO, dc::DataCollection)
     datakeygen(key) = if haskey(DATA_CONFIG_KEY_SORT_MAPPING, key)
         [DATA_CONFIG_KEY_SORT_MAPPING[key]]
     else
         natkeygen(key)
     end
-    TOML.print(io, filter(((_, value),) -> !isnothing(value) && !isempty(value),
-                          convert(Dict, dc));
+    intermediate = IOBuffer()
+    TOML.print(intermediate,
+               filter(((_, value),) -> !isnothing(value) && !isempty(value),
+                      convert(Dict, dc));
                sorted = true, by = datakeygen)
+    write(io, take!(tomlreformat!(intermediate)))
 end
 
 function Base.write(dc::DataCollection)
