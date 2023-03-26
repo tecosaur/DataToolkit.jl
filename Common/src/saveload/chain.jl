26d2b46016813ea08c1ecfc984699bd61bc62a28
TEC <git@tecosaur.net>
2023-03-26 22:48:14 +0800

    tweak(chain): use the new @advise macro
    Might as well, for consistency.

--- Common/src/saveload/chain.jl
+++ Common/src/saveload/chain.jl
@@ -123,8 +123,8 @@ function create(::Type{DataLoader{:chain}}, source::String, dataset::DataSet)
             if !isnothing(innerloader)
                 minimalspec(template) = Dict{String, Any}(
                     "driver" => template["driver"])
-                ospec = dataset.collection.advise(tospec, outerloader)
-                ispec = dataset.collection.advise(tospec, innerloader)
+                ospec = DataToolkitBase.@advise collection tospec(outerloader)
+                ispec = DataToolkitBase.@advise collection tospec(innerloader)
                 Dict{String, Any}("loaders" =>
                     if ospec == minimalspec(ospec) && ispec == minimalspec(ispec)
                         [ospec["driver"], ispec["driver"]]
