05002e86582ccf0746c141b9b507da49790ab182
TEC <git@tecosaur.net>
2023-03-30 09:33:31 +0800

    nit(repl): order plugins alphabetically

--- Common/src/repl/repl.jl
+++ Common/src/repl/repl.jl
@@ -23,28 +23,28 @@ include("show.jl")
 
 function add_repl_cmds()
     push!(REPL_CMDS,
+          ReplCmd(:add, ADD_DOC, add),
+          ReplCmd(:delete, DELETE_DOC, delete),
           ReplCmd(:init, INIT_DOC, init),
-          ReplCmd(:stack,
-                  "Operate on the data collection stack",
-                  STACK_SUBCOMMANDS),
-          ReplCmd(:plugin,
-                  "Inspect and modify the set of plugins used
-
-Call without any arguments to see the availible subcommands.",
-                  PLUGIN_SUBCOMMANDS),
           ReplCmd(:config,
                   "Inspect and modify the current configuration",
                   CONFIG_SUBCOMMANDS),
-          ReplCmd(:add, ADD_DOC, add),
-          ReplCmd(:delete, DELETE_DOC, delete),
           ReplCmd(:list,
                   "List the datasets in a certain collection
 
 By default, the datasets of the active collection are shown.",
                   repl_list),
+          ReplCmd(:plugin,
+                  "Inspect and modify the set of plugins used
+
+Call without any arguments to see the availible subcommands.",
+                  PLUGIN_SUBCOMMANDS),
           ReplCmd(:show,
                   "List the dataset refered to by an identifier",
-                  repl_show))
+                  repl_show),
+          ReplCmd(:stack,
+                  "Operate on the data collection stack",
+                  STACK_SUBCOMMANDS))
 end
 
 end
