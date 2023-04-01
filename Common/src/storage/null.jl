28cdb0cb51d1834056ba30c935f48fdb4d85d21c
TEC <git@tecosaur.net>
2023-04-01 17:04:34 +0800

    fix(null): potential method ambiguity

--- Common/src/storage/null.jl
+++ Common/src/storage/null.jl
@@ -1,3 +1,6 @@
 # This is a special kind of store that's basically just needed for
 # the `:julia` loader.
 storage(::DataStorage{:null}, ::Any; write::Bool) = Some(nothing)
+
+# To avoid method ambiguity
+storage(::DataToolkitBase.DataStorage{:null}, ::Type; write::Bool) = Some(nothing)
