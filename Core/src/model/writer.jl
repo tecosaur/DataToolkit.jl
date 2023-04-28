b0bcab9f9c3a168bd90b9ccb55402abb67ccbc24
TEC <git@tecosaur.net>
2023-04-29 00:43:31 +0800

    fix(writer): include Base./Core. in non-exported
    This should be a bit safer

--- Core/src/model/writer.jl
+++ Core/src/model/writer.jl
@@ -20,7 +20,9 @@ function Base.string(q::QualifiedType)
     if haskey(QUALIFIED_TYPE_SHORTHANDS.reverse, q)
         return QUALIFIED_TYPE_SHORTHANDS.reverse[q]
     end
-    qname = if q.parentmodule in (:Base, :Core)
+    qname = if q.parentmodule == :Base && Base.isexported(Base, q.name)
+        string(q.name)
+    elseif q.parentmodule == :Core && Base.isexported(Core, q.name)
         string(q.name)
     else
         string(q.parentmodule, '.', q.name)
