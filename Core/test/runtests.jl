e8877ef4f2a7a181c7e54d2801e19e8bf8f4643d
TEC <git@tecosaur.net>
2023-05-10 23:03:49 +0800

    test(utils): add test set

--- Core/test/runtests.jl
+++ Core/test/runtests.jl
@@ -1,6 +1,42 @@
 using DataToolkitBase
 using Test
 
-@testset "DataToolkitBase.jl" begin
-    # Write your tests here.
+import DataToolkitBase: natkeygen, stringdist, stringsimilarity,
+    longest_common_subsequence, highlight_lcs
+
+@testset "Utils" begin
+    @testset "Doctests" begin
+        @test natkeygen.(["A1", "A10", "A02", "A1.5"]) ==
+            [["a", "0\x01"], ["a", "0\n"], ["a", "0\x02"], ["a", "0\x015"]]
+        @test sort(["A1", "A10", "A02", "A1.5"], by=natkeygen) ==
+            ["A1", "A1.5", "A02", "A10"]
+        @test stringdist("The quick brown fox jumps over the lazy dog",
+                        "The quack borwn fox leaps ovver the lzy dog") == 7
+        @test stringdist("typo", "tpyo") == 1
+        @test stringdist("frog", "cat") == 4
+        @test stringsimilarity("same", "same") == 1.0
+        @test stringsimilarity("semi", "demi") == 0.75
+        @test longest_common_subsequence("same", "same") == [1:4;]
+        @test longest_common_subsequence("fooandbar", "foobar") == vcat(1:3, 7:9)
+    end
+    @testset "Multi-codepoint unicode" begin
+        @test stringdist("ÆaÆb", "ÆacÆb") == 1
+        @test stringsimilarity("ÆaÆb", "ÆacÆb") == 0.8
+        @test longest_common_subsequence("heyÆsop", "hiÆsop") == vcat(1, 4:7)
+    end
+    @testset "Highlighting LCS" begin
+        io = IOContext(IOBuffer(), :color => true)
+        highlight_lcs(io, "hey", "hey")
+        @test String(take!(io.io)) == "\e[1mhey\e[22m"
+        highlight_lcs(io, "hey", "hey", invert=true)
+        @test String(take!(io.io)) == "hey\e[22m"
+        highlight_lcs(io, "hey", "hey", before="^", after="_")
+        @test String(take!(io.io)) == "^hey_"
+        highlight_lcs(io, "xxheyyy", "aaheybb")
+        @test String(take!(io.io)) == "xx\e[1mhey\e[22myy\e[22m"
+        highlight_lcs(io, "xxheyyy", "aaheybb", before="^", after="_", invert=true)
+        @test String(take!(io.io)) == "^xx_hey^yy_"
+        highlight_lcs(io, "xxheyyy", "xx___yy")
+        @test String(take!(io.io)) == "\e[1mxx\e[22mhey\e[1myy\e[22m"
+    end
 end
