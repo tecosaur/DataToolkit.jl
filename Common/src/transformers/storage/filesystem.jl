1fd0d0e1f987fdd3db0ebf4f501a12c9e4385699
TEC <git@tecosaur.net>
2023-07-12 01:19:16 +0800

    feat(filesystem): better integrate with the store

--- Common/src/transformers/storage/filesystem.jl
+++ Common/src/transformers/storage/filesystem.jl
@@ -35,7 +35,61 @@ function create(::Type{<:DataStorage{:filesystem}}, source::String, dataset::Dat
     end
 end
 
-Store.shouldstore(::DataStorage{:filesystem}) = false
+# We want to tweak the result of `rhash` to take into account the `mtime` of the
+# file if there is no checksum.
+function Store.rhash(storage::DataStorage{:filesystem}, h::UInt)
+    if @getparam(storage."checksum"::Union{Bool, String}, false) === false
+        h = hash(if isfile(@getparam storage."path"::String)
+                     mtime(@getparam storage."path"::String)
+                 else 0.0 end, h)
+    else
+        # The checksum should already be accounted for since it's a storage parameter,
+        # but that means we should omit the path.
+        storage = DataStorage{:filesystem}(
+            storage.dataset, storage.type, storage.priority,
+            delete!(copy(storage.parameters), "path"))
+    end
+    invoke(Store.rhash, Tuple{AbstractDataTransformer, UInt}, storage, h)
+end
+
+# Variant on the generic `storesave` implementation that copies the file.
+# Instead, we create a symlink so we can make use of the checksum metadata.
+# We just need to check the symlink is no older than the original file.
+function Store.storesave(inventory::Store.Inventory, storage::DataStorage{:filesystem}, ::Type{FilePath}, file::FilePath)
+    checksum = Store.getchecksum(storage, file.path)
+    newsource = Store.StoreSource(
+        Store.rhash(storage),
+        [storage.dataset.collection.uuid],
+        now(), checksum, last(splitext(file.path))[2:end])
+    linkfile = Store.storefile(inventory, newsource)
+    isfile(linkfile) && rm(linkfile)
+    symlink(file.path, linkfile)
+    Store.update_source!(inventory, newsource, storage.dataset.collection)
+    linkfile
+end
+
+# Similarly, we need a variant on the generic `storefile` implementation to check
+# the symlink and pre-emptively delete it if the actual file is newer. This will
+# now trigger `storesave` again.
+function Store.storefile(inventory::Store.Inventory, storage::DataStorage{:filesystem})
+    source = Store.getsource(inventory, storage)
+    if !isnothing(source)
+        linkfile = Store.storefile(inventory, source)
+        if isfile(linkfile)
+            file = getpath(storage)
+            if isfile(file) && mtime(linkfile) > mtime(file)
+                return linkfile
+            else
+                rm(linkfile)
+            end
+        end
+        # Symlink never existed, or has been removed, so ensure
+        # no associated store entry exists.
+        index = findfirst(==(source), inventory.stores)
+        !isnothing(index) && deleteat!(inventory.stores, index)
+        nothing
+    end
+end
 
 const FILESYSTEM_DOC = md"""
 Read and write access to local files
