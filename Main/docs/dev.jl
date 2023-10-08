769632b7c39f0cdb3482b2f4c1244632b02e2028
TEC <git@tecosaur.net>
2023-10-08 15:55:58 +0800

    docs: upgrade to Documenter 1.0

--- Main/docs/dev.jl
+++ Main/docs/dev.jl
@@ -1,2 +1,28 @@
+#!/usr/bin/env -S julia --startup-file=no
+
+function cleanup()
+    rm(joinpath(@__DIR__, "build"), force=true, recursive=true)
+    for (root, _, files) in walkdir(joinpath(@__DIR__, "src"))
+        for file in files
+            if endswith(file, ".md")
+                rm(joinpath(root, file))
+            end
+        end
+    end
+end
+cleanup()
+
+using Pkg
+Pkg.activate(@__DIR__)
+Pkg.develop(PackageSpec(; path=dirname(@__DIR__)))
+Pkg.instantiate()
+
 using LiveServer
-servedocs(doc_env=true, foldername=@__DIR__)
+
+Base.exit_on_sigint(false)
+try
+    servedocs(doc_env=true, foldername=@__DIR__)
+finally
+    Pkg.rm(PackageSpec(; path=dirname(@__DIR__)))
+    cleanup()
+end
