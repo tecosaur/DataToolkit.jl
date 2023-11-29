95ee5cdf129697c5bf5e5be4673c3e07dfd7041e
TEC <git@tecosaur.net>
2023-11-29 22:56:46 +0800

    feat(saveload): revamp io->file loader
    It's now able to more adroitly handle the file->file case, and cover a
    potential edge case at the cost of (often unnecessary) extra work (on an
    opt-in basis).

--- Common/src/transformers/saveload/iotofile.jl
+++ Common/src/transformers/saveload/iotofile.jl
@@ -1,10 +1,28 @@
-function load(loader::DataLoader{Symbol("io->file")}, from::IO, ::Type{FilePath})
-    path = abspath(dirof(loader.dataset.collection),
-                   @something(expanduser(@getparam loader."path"::Union{String, Nothing}),
-                              joinpath(tempdir(),
-                                       string("julia_datatoolkit_iotofile_",
-                                              loader.dataset.uuid))))
+function _iotofilepath(loader::DataLoader)
+    pathattr = @getparam loader."path"::Union{String, Nothing}
+    path = if !isnothing(pathattr)
+        expanduser(pathattr)
+    else
+        joinpath(tempdir(), string("julia_datatoolkit_iotofile_", loader.dataset.uuid))
+    end
+    abspath(dirof(loader.dataset.collection), path)
+end
+
+function load(loader::DataLoader{Symbol("io->file")}, from::FilePath, ::Type{FilePath})
+    path = _iotofilepath(loader)
+    # Remove non-symlinks, broken symlinks, and incorrect symlinks
+    if isfile(path) && (!islink(path) || !isfile(realpath(path)) || realpath(path) != abspath(from.path))
+        rm(path)
+    end
     if !isfile(path)
+        symlink(from.path, path)
+    end
+    FilePath(path)
+end
+
+function load(loader::DataLoader{Symbol("io->file")}, from::IO, ::Type{FilePath})
+    path = _iotofilepath(loader)
+    if !isfile(path) || @getparam loader."rewrite"::Bool false
         open(path, "w") do io
             write(io, from)
         end
@@ -18,6 +36,14 @@ Obtain an IO as a FilePath
 The `io->file` loader serves as a bridge between and backends that produce IO
 but not a file path, and any subsequent transformers that require a file.
 
+If a `FilePath` can be provided directly, `io->file` will be sneaky and just
+create a symlink.
+
+!!! warn
+    If a file with the given path already exists, it is possible for the content
+    to become out of date, set `rewrite` to write the file every access and so
+    avoid this potential issue. This is not a risk in the symlink case.
+
 # Input/output
 
 The `io->file` driver accepts `IO` and produces a `FilePath`.
@@ -25,4 +51,5 @@ The `io->file` driver accepts `IO` and produces a `FilePath`.
 # Parameters
 
 - `path`: A path to save the file to. If not set, a tempfile will be used.
+- `rewrite`: Whether the any existing file should be overwritten afresh on each access.
 """
