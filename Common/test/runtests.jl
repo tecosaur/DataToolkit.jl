6298de270062366ba385b1090bce6769d8fb20ed
TEC <git@tecosaur.net>
2024-02-04 19:43:20 +0800

    test: add tests for many transformers

--- Common/test/runtests.jl
+++ Common/test/runtests.jl
@@ -1,6 +1,76 @@
+using DataToolkitBase
 using DataToolkitCommon
+using DataFrames
 using Test
 
-@testset "DataToolkitCommon.jl" begin
-    # Write your tests here.
+DataToolkitBase.loadcollection!("Data.toml")
+
+@testset "Storage" begin
+end
+
+@testset "Loaders/Writers" begin
+    @testset "arrow" begin
+        @test size(read(dataset("iris-arrow"))) == (150, 5)
+    end
+    @testset "compression" begin
+        iris = read(dataset("iris"), Matrix)
+        @test read(dataset("iris-bzip2"), Matrix) == iris
+        @test read(dataset("iris-gz"), Matrix) == iris
+        @test read(dataset("iris-xz"), Matrix) == iris
+        @test read(dataset("iris-zstd"), Matrix) == iris
+    end
+    @testset "csv" begin
+        @test size(read(dataset("iris"), DataFrame)) == (150, 5)
+    end
+    @testset "jpeg" begin
+        @test read(dataset("lighthouse-jpeg"), Matrix) isa Matrix
+    end
+    @testset "jld2" begin
+        @test size(read(dataset("iris-jld2"))) == (150, 5)
+    end
+    @testset "netpbm" begin
+        # Currently broken, see <https://github.com/JuliaIO/Netpbm.jl/issues/39>
+        # @test read(dataset("lighthouse-netpbm"), Matrix) isa Matrix
+    end
+    @testset "png" begin
+        @test read(dataset("lighthouse-png"), Matrix) isa Matrix
+    end
+    @testset "qoi" begin
+        @test read(dataset("lighthouse-qoi"), Matrix) isa Matrix
+    end
+    @testset "tar" begin
+        @test sum(Vector{UInt8}(read(dataset("iris-tar"), String))) == 258587
+    end
+    @testset "tiff" begin
+        @test read(dataset("lighthouse-tiff"), AbstractMatrix) isa AbstractMatrix
+    end
+    @testset "zip" begin
+        @test sum(read(read(dataset("iris-zip"), IO))) == 258587
+    end
+end
+
+@testset "Plugins" begin
+    @testset "Cache" begin
+    end
+    @testset "Defaults" begin
+    end
+    @testset "Log" begin
+    end
+    @testset "Memorise" begin
+    end
+    @testset "Store" begin
+        @testset "Checksums" begin
+            val = "Aren't checksums neat?\n"
+            @test read(dataset("checksum-k12"),    String) == val
+            @test read(dataset("checksum-crc32c"), String) == val
+            @test read(dataset("checksum-md5"),    String) == val
+            @test read(dataset("checksum-sha1"),   String) == val
+            @test read(dataset("checksum-sha224"), String) == val
+            @test read(dataset("checksum-sha256"), String) == val
+            @test read(dataset("checksum-sha384"), String) == val
+            @test read(dataset("checksum-sha512"), String) == val
+        end
+    end
+    @testset "Versions" begin
+    end
 end
