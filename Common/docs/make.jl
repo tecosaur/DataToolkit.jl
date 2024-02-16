41d2f67dd84d97468307b909fb5e4273af373eb2
TEC <git@tecosaur.net>
2024-02-16 13:38:46 +0800

    docs: fix soruce and repo links
    We update the docs make.jl recipe to match that of DataToolkit after
    https://github.com/tecosaur/DataToolkit.jl/commit/c7e954744bbaa370a2c24630480f51e0aa0ddbd4.

--- Common/docs/make.jl
+++ Common/docs/make.jl
@@ -3,16 +3,28 @@ using DataToolkitCommon
 using Markdown
 using Org
 
-for (root, _, files) in walkdir(joinpath(@__DIR__, "src"))
-    orgfiles = joinpath.(root, filter(f -> endswith(f, ".org"), files))
-    for orgfile in orgfiles
-        mdfile = replace(orgfile, r"\.org$" => ".md")
-        read(orgfile, String) |>
-            c -> Org.parse(OrgDoc, c) |>
-            o -> sprint(markdown, o) |>
-            s -> replace(s, r"\.org]" => ".md]") |>
-            m -> write(mdfile, m)
+let orgconverted = 0
+    html2utf8entity_dirty(text) = # Remove as soon as unnecesary
+        replace(text,
+                "&hellip;" => "…",
+                "&mdash;" => "—",
+                "&mdash;" => "–",
+                "&shy;" => "-")
+    for (root, _, files) in walkdir(joinpath(@__DIR__, "src"))
+        orgfiles = joinpath.(root, filter(f -> endswith(f, ".org"), files))
+        for orgfile in orgfiles
+            mdfile = replace(orgfile, r"\.org$" => ".md")
+            read(orgfile, String) |>
+                c -> Org.parse(OrgDoc, c) |>
+                o -> sprint(markdown, o) |>
+                html2utf8entity_dirty |>
+                s -> replace(s, r"\.org]" => ".md]") |>
+                m -> string("```@meta\nEditURL=\"$(basename(orgfile))\"\n```\n\n", m) |>
+                m -> write(mdfile, m)
+        end
+        orgconverted += length(orgfiles)
     end
+    @info "Converted $orgconverted files from .org to .md"
 end
 
 Core.eval(DataToolkitCommon,
